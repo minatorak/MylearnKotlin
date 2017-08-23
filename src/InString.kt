9ddbf6da6d2cb6109273249fc90e7651ff35242a
sirirak <hotmail22866@gmail.com>
2017-08-23 16:04:11 +0700

    quick Lecture 164

---
+++ src/InString.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    var title=" welcome to new york"
+    var Text="title:$title"
+    println(Text)
+    println(Text[3])
+    println(Text.toUpperCase())
+    println(Text.toLowerCase())
+    println(Text.length)
+    println(Text.split(" "))
+    println(Text.trim())
+    
+}
