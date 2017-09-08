8339a7b7ccea22955bc9a8611d52c9d1dfda735d
sirirak <hotmail22866@gmail.com>
2017-09-08 07:46:19 +0700

    Lecture 187 add Extensions

--- src/MySourceCode/FunctionExtensions.kt
+++ src/MySourceCode/FunctionExtensions.kt
@@ -1,2 +1,16 @@
 package MySourceCode
 
+fun ArrayList<String>.mswap(index1:Int,index2: Int){
+    var temp = this.get(index1)
+    this.set(index1,this.get(index2))
+    this.set(index2,temp)
+}
+fun main(args: Array<String>) {
+    var arr = ArrayList<String>()
+    arr.add("god")
+    arr.add("evil")
+    arr.add("love")
+    println(arr)
+    arr.mswap(1,2)
+    println(arr)
+}
