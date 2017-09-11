313e8903584696b1ad22290594b35eecf381ee12
sirirak <hotmail22866@gmail.com>
2017-09-12 04:51:06 +0700

    update refactor

--- src/MySourceCode/DataType.kt
+++ src/MySourceCode/DataType.kt
@@ -1,2 +1,12 @@
 package MySourceCode
 
+fun main(args: Array<String>) {
+    val int = 1234
+    val long = 123456789L
+    val double = 12.34
+    val float = 12.34F
+    val hexadecimal = 0xAB7
+    val binary = 0b01011010
+
+
+}
